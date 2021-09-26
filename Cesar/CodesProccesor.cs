9c3ed4be2c262c0b5ec279ccf58946776a2cb27d
Andrii Suvorov <ammatori6395@gmail.com>
2021-09-26 12:05:50 +0300

    Add project files.

---
+++ Cesar/CodesProccesor.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Cesar
+{
+    class CodesProccesor
+    {
+        public string Encode(string toChar)
+        {
+            Random rnd = new Random();
+            int code = rnd.Next(-5, 10);
+            char[] toString;
+            toString = toChar.ToCharArray();
+            for (int i = 0; i < toString.Length; i++)
+            {
+                toString[i] = (char)((int)toChar[i] + code);
+            }
+            toChar = new string(toString);
+            int position = rnd.Next(1, toChar.Length);
+            toChar = toChar.Insert(position, "&/"+code+"hg");
+            return toChar;
+        }
+
+        public string Decode(string toChar)
+        {
+            string sPat = (@"&/\d+hg");
+            if (Regex.IsMatch(toChar, sPat) )
+            {
+                int decodeKey = Int32.Parse(Regex.Match(toChar, @"\d+").Value);
+                string toCut = "&/"+decodeKey.ToString()+"hg";
+                toChar = toChar.Replace(toCut, "");
+                char[] toString;
+                toString = toChar.ToCharArray();
+                for (int i = 0; i < toString.Length; i++)
+                {
+                    toString[i] = (char)((int)toChar[i] - decodeKey);
+                }
+                toChar = new string(toString);
+            }
+            return toChar;
+        }
+    }
+}
---
+++ Cesar/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Cesar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string forCod;
+            CodesProccesor proc = new CodesProccesor();
+            Console.WriteLine("Enter to encode");
+            forCod = Console.ReadLine();
+            forCod = proc.Encode(forCod);
+            Console.WriteLine(forCod);
+            Console.WriteLine("and back");
+            forCod = proc.Decode(forCod);
+            Console.WriteLine(forCod);
+        }
+    }
+}
